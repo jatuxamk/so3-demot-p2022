46156baf8c503640ebfc3df2d054ed7bb741043b
Janne Turunen <janne.turunen@xamk.fi>
2022-09-01 10:34:28 +0300

    Kertausta

--- demo00/routes/apiTehtavat.ts
+++ demo00/routes/apiTehtavat.ts
@@ -9,45 +9,117 @@ apiTehtavatRouter.use(express.json());
 
 apiTehtavatRouter.delete("/:id", async (req : express.Request, res : express.Response) => {
 
-    await prisma.tehtava.delete({
-        where : {
-            id : Number(req.params.id)
-        }
-    })
+    if (await prisma.tehtava.count({
+            where : {
+                id : Number(req.params.id)
+            }
+    })) {
 
-    res.json(await prisma.tehtava.findMany());
+        try {
+
+            await prisma.tehtava.delete({
+                where : {
+                    id : Number(req.params.id)
+                }
+            })
+        
+            res.json(await prisma.tehtava.findMany());
+
+        } catch (e : any) {
+
+            res.status(500).json({"virhe" : "Palvelimella tapahtui odottamaton virhe."})
+
+        }
+
+    } else {
+
+        res.status(400).json({ "virhe" : "Virheellinen id."});
+
+    }
+
+
+
 
 });
 
 apiTehtavatRouter.put("/:id", async (req : express.Request, res : express.Response) => {
 
-    await prisma.tehtava.update({
+    if (await prisma.tehtava.count({
         where : {
-                id : Number(req.params.id)
-            },
-        data: {
-            nimi : req.body.nimi,
-            suoritettu : Boolean(req.body.suoritettu)
+            id : Number(req.params.id)
         }
-    });
+    })) {
 
-    res.json(await prisma.tehtava.findMany());
+        try {
+
+            await prisma.tehtava.update({
+                where : {
+                        id : Number(req.params.id)
+                    },
+                data: {
+                    nimi : req.body.nimi,
+                    suoritettu : Boolean(req.body.suoritettu)
+                }
+            });
+    
+            res.json(await prisma.tehtava.findMany());
+    
+        } catch (e : any) {
+
+            res.status(500).json({"virhe" : "Palvelimella tapahtui odottamaton virhe."})
+
+        }
+
+    } else {
+
+        res.status(400).json({ "virhe" : "Virheellinen id."});
+
+    }   
 
 });
 
 apiTehtavatRouter.post("/", async (req : express.Request, res : express.Response) => {
 
-    await prisma.tehtava.create({
-        data: req.body
-    });
+    if (Boolean(req.body.nimi) && (Boolean(req.body.suoritettu) === true || Boolean(req.body.suoritettu === false))) {
 
-    res.json(await prisma.tehtava.findMany());
+        try {
+
+            await prisma.tehtava.create({
+                data: {
+                        nimi : req.body.nimi,
+                        suoritettu : Boolean(req.body.suoritettu)
+                }
+            });
+        
+            res.json(await prisma.tehtava.findMany());
+
+        } catch (e : any) {
+
+            res.status(500).json({"virhe" : "Palvelimella tapahtui odottamaton virhe."})
+
+        }
+
+    } else {
+
+        res.status(400).json({ "virhe" : "Virheelinen pyynnön body" });
+
+    }
+
 
 });
 
 apiTehtavatRouter.get("/", async (req : express.Request, res : express.Response) => {
 
-    res.json(await prisma.tehtava.findMany());
+    try {
+
+        res.json(await prisma.tehtava.findMany());
+
+    } catch (e : any) {
+
+        res.status(500).json({"virhe" : "Palvelimella tapahtui odottamaton virhe."})
+
+    }
+    
 
 });
 
