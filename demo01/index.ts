090d2adbe20cc07c2a001288182d87df6b5920ef
Janne Turunen <janne.turunen@xamk.fi>
2022-09-06 14:54:32 +0300

    EJS-templatet

--- demo01/index.ts
+++ demo01/index.ts
@@ -1,4 +1,8 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+import { brotliDecompressSync } from "zlib";
+
+const prisma : PrismaClient = new PrismaClient();
 
 const app : express.Application = express();
 
@@ -6,9 +10,56 @@ const portti : number = Number(process.env.PORT) || 3001;
 
 app.set("view engine", "ejs");
 
-app.get("/", (req : express.Request, res : express.Response) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
 
-    res.render("index");
+app.post("/poista", async (req : express.Request, res : express.Response) => {
+
+    await prisma.tehtava.delete({
+        where : {
+            id : Number(req.body.id)
+        }
+    });
+
+    res.redirect("/");
+
+});
+
+app.post("/", async (req : express.Request, res : express.Response) => {
+
+    await prisma.tehtava.create({
+        data : {
+            nimi : req.body.nimi || "Nimetön tehtävä",
+            suoritettu : false
+        }
+    });
+
+    let tehtavat = await prisma.tehtava.findMany();
+
+    res.render("index", { tehtavat : tehtavat });
+
+});
+
+app.get("/suoritettu", async (req : express.Request, res : express.Response) => {
+
+    await prisma.tehtava.update({
+        where : {
+            id : Number(req.query.id)
+        },
+        data : {
+            suoritettu : (req.query.suoritettu === "true") ? false : true
+        }
+    });
+
+    res.redirect("/");
+
+});
+
+app.get("/", async (req : express.Request, res : express.Response) => {
+
+    let tehtavat = await prisma.tehtava.findMany();
+
+    res.render("index", { tehtavat : tehtavat });
 
 });
 
